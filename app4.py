4990f6076c2a1960cfbeda66eba9c5079aaa6517
Tamás Jákob <36410743+mecalis@users.noreply.github.com>
2025-10-29 13:10:13 +0100

    Update app4.py

--- app4.py
+++ app4.py
@@ -15,7 +15,7 @@ def main():
     st.set_page_config(page_title="Kérdezz valamit a PDF-től!")
     st.header("Kérdezz valamit a PDF-től! 💬")
     st.write("Kérdés javaslat1: Ez a szöveg egy elektronikus számla. Keresd meg benne a kiszámlázott tételek nevét, darabszámát és az árát.")
-    st.write("Kérdés javaslat2: Ez a szöveg egy elektronikus számla. Keresd meg benne a számla dátumát, a kiszámlázott tételek nevét, darabszámát és az árát. Az értékeket az alábbi formátumban add vissza, hogy tételenként: "számla dátuma - megnevezés - mennyiség - egységár")
+    st.write("Kérdés javaslat2: Ez a szöveg egy elektronikus számla. Keresd meg benne a számla dátumát, a kiszámlázott tételek nevét, darabszámát és az árát. Az értékeket az alábbi formátumban add vissza, hogy tételenként: számla dátuma - megnevezés - mennyiség - egységár")
     openai_api_key = st.secrets["OPENAI_API_KEY"]
     # upload file
     pdf = st.file_uploader("Tölts fel ide egy PDF-et!", type="pdf")
@@ -57,6 +57,7 @@ def main():
 if __name__ == '__main__':
 
     main()
+
 
 
 
